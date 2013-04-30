068d18863b4281fecd2b67b289c9f20fa98924f4
Michael Warkentin <mwarkentin@gmail.com>
2013-04-29 23:35:45 -0400

    Add payload to test and switch back to colourio

--- iron-worker/shell-test/shell.py
+++ iron-worker/shell-test/shell.py
@@ -1,4 +1,22 @@
+import json
+import sys
+
 import envoy
+
+payload_file = None
+payload = None
+
+for i in range(len(sys.argv)):
+    if sys.argv[i] == "-payload" and (i + 1) < len(sys.argv):
+        payload_file = sys.argv[i + 1]
+        with open(payload_file, 'r') as f:
+            payload = json.loads(f.read())
+        break
+
+print payload
+
+cmd = './test.sh {}'.format(payload['repo'])
+print cmd
 
 r = envoy.run('./test.sh')
 print r.std_out
