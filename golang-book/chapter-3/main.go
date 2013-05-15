1012114b3d2ed125d6be0f5ceb9777184037957e
Michael Warkentin <mwarkentin@gmail.com>
2013-05-15 12:11:55 -0400

    Chapter 3, step 2

--- golang-book/chapter-3/main.go
+++ golang-book/chapter-3/main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("1 + 1 =", 1.0 + 1.0)
+    fmt.Println(len("Hello world"))
+    fmt.Println("Hello world"[1])
+    fmt.Println("Hello " + "world")
 }
