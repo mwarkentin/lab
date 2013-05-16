9d858bee1cb996b7ead6863b4ea5e50abc7d2be3
Michael Warkentin <mwarkentin@gmail.com>
2013-05-16 14:24:46 -0400

    Chapter 5, step 2

--- golang-book/chapter-5/main.go
+++ golang-book/chapter-5/main.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
     for i := 1; i <= 10; i++ {
-        fmt.Println(i)
+        if i % 2 == 0 {
+            fmt.Println(i, "even")
+        } else {
+            fmt.Println(i, "odd")
+        }
     }
 }
