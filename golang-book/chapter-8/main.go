9d88a5c71ccb5b95a9539675cc5d1f6cc24604a6
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 15:55:10 -0400

    Chapter 8, step 2

--- golang-book/chapter-8/main.go
+++ golang-book/chapter-8/main.go
@@ -6,8 +6,14 @@ func zero(xPtr *int) {
     *xPtr = 0
 }
 
+func one(xPtr *int) {
+    *xPtr = 1
+}
+
 func main() {
-    x := 5
-    zero(&x)
-    fmt.Println(x)
+    xPtr := new(int)
+    zero(xPtr)
+    fmt.Println(*xPtr)
+    one(xPtr)
+    fmt.Println(*xPtr)
 }
