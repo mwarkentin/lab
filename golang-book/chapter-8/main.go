dd093ec1985e70fab6eadaf485d5ec0bd17a324a
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 15:58:49 -0400

    Chapter 8, step 3

--- golang-book/chapter-8/main.go
+++ golang-book/chapter-8/main.go
@@ -10,10 +10,18 @@ func one(xPtr *int) {
     *xPtr = 1
 }
 
+func square(x *float64) {
+    *x = *x * *x
+}
+
 func main() {
     xPtr := new(int)
     zero(xPtr)
     fmt.Println(*xPtr)
     one(xPtr)
     fmt.Println(*xPtr)
+
+    x := 1.5
+    square(&x)
+    fmt.Println(x)
 }
