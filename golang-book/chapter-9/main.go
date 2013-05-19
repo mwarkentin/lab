444a7e1993cd6e245f0bd17b2cde1e36f573c459
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 19:38:17 -0400

    Chapter 9, step 2

--- golang-book/chapter-9/main.go
+++ golang-book/chapter-9/main.go
@@ -2,6 +2,10 @@ package main
 
 import ("fmt"; "math")
 
+type Circle struct {
+    x, y, r float64
+}
+
 func distance(x1, y1, x2, y2 float64) float64 {
     a := x2 - x1
     b := y2 - y1
@@ -14,15 +18,15 @@ func rectangleArea(x1, y1, x2, y2 float64) float64 {
     return l * w
 }
 
-func circleArea(x, y, r float64) float64 {
-    return math.Pi * r*r
+func circleArea(c *Circle) float64 {
+    return math.Pi * c.r*c.r
 }
 
 func main() {
     var rx1, ry1 float64 = 0, 0
     var rx2, ry2 float64 = 10, 10
-    var cx, cy, cr float64 = 0, 0, 5
+    c := Circle{0, 0, 5}
 
     fmt.Println(rectangleArea(rx1, ry1, rx2, ry2))
-    fmt.Println(circleArea(cx, cy, cr))
+    fmt.Println(circleArea(&c))
 }
