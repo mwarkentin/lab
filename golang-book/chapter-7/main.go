a938663bc21ec091de161795f1b9ea79b35756ef
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 15:34:58 -0400

    Chapter 7, step 2

--- golang-book/chapter-7/main.go
+++ golang-book/chapter-7/main.go
@@ -18,8 +18,24 @@ func add(args ...int) int {
     return total
 }
 
+func sum(xs []float64) float64 {
+    total := 0.0
+    for _, v := range xs {
+        total += v
+    }
+    return total
+}
+
+func half(num int) (int, bool) {
+    half := num / 2
+    isEven := num % 2 == 0
+    return half, isEven
+}
+
 func main() {
     xs := []float64{98,93,77,82,83}
     fmt.Println(average(xs))
+    fmt.Println(sum(xs))
     fmt.Println(add(1,2,3))
+    fmt.Println(half(1))
 }
