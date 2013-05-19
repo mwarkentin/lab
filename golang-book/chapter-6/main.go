342256bf8a19135a9644084eb2b9a5a9392b4a4d
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 14:30:28 -0400

    Chapter 6, step 8

--- golang-book/chapter-6/main.go
+++ golang-book/chapter-6/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
     slice1 := []int{1,2,3}
-    slice2 := append(slice1, 4, 5)
+    slice2 := make([]int, 2)
+    copy(slice2, slice1)
     fmt.Println(slice1, slice2)
 }
