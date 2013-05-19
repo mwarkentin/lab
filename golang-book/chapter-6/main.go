379e9ec7906da06188cec105e6bd3c53b2357531
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 14:34:33 -0400

    Chapter 6, step 10

--- golang-book/chapter-6/main.go
+++ golang-book/chapter-6/main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-    x := make(map[string]int)
-    x["key"] = 10
-    fmt.Println(x["key"])
+    elements := make(map[string]string)
+    elements["H"] = "Hydrogen"
+    elements["He"] = "Helium"
+    elements["Li"] = "Lithium"
+    elements["Be"] = "Beryllium"
+    elements["B"] = "Boron"
+    elements["C"] = "Carbon"
+    elements["N"] = "Nitrogen"
+    elements["O"] = "Oxygen"
+    elements["F"] = "Fluorine"
+    elements["Ne"] = "Neon"
+
+    fmt.Println(elements["Li"])
 }
