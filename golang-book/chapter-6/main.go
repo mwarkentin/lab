5052175bcdb75f1f7b02257f1e389850ab704d72
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 14:40:32 -0400

    Chapter 6, step 13

--- golang-book/chapter-6/main.go
+++ golang-book/chapter-6/main.go
@@ -3,19 +3,20 @@ package main
 import "fmt"
 
 func main() {
-    elements := make(map[string]string)
-    elements["H"] = "Hydrogen"
-    elements["He"] = "Helium"
-    elements["Li"] = "Lithium"
-    elements["Be"] = "Beryllium"
-    elements["B"] = "Boron"
-    elements["C"] = "Carbon"
-    elements["N"] = "Nitrogen"
-    elements["O"] = "Oxygen"
-    elements["F"] = "Fluorine"
-    elements["Ne"] = "Neon"
+    elements := map[string]string{
+        "H": "Hydrogen",
+        "He": "Helium",
+        "Li": "Lithium",
+        "Be": "Beryllium",
+        "B": "Boron",
+        "C": "Carbon",
+        "N": "Nitrogen",
+        "O": "Oxygen",
+        "F": "Fluorine",
+        "Ne": "Neon",
+    }
 
-    if name, ok := elements["Un"]; ok {
+    if name, ok := elements["Be"]; ok {
         fmt.Println(name, ok)
     }
 }
