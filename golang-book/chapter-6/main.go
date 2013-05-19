40e56e7104bef18780699a4430d6738f27c79440
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 14:29:41 -0400

    Chapter 6, step 7

--- golang-book/chapter-6/main.go
+++ golang-book/chapter-6/main.go
@@ -3,17 +3,7 @@ package main
 import "fmt"
 
 func main() {
-    x := [5]float64{
-        98,
-        93,
-        77,
-        82,
-        83,
-    }
-
-    var total float64 = 0
-    for _, value := range x {
-        total += value
-    }
-    fmt.Println(total / float64(len(x)))
+    slice1 := []int{1,2,3}
+    slice2 := append(slice1, 4, 5)
+    fmt.Println(slice1, slice2)
 }
