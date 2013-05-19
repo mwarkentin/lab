c0b8f3b3463df97d0580cc767869dd5787dddfaf
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 14:44:04 -0400

    Chapter 6, step 14

--- golang-book/chapter-6/main.go
+++ golang-book/chapter-6/main.go
@@ -3,20 +3,50 @@ package main
 import "fmt"
 
 func main() {
-    elements := map[string]string{
-        "H": "Hydrogen",
-        "He": "Helium",
-        "Li": "Lithium",
-        "Be": "Beryllium",
-        "B": "Boron",
-        "C": "Carbon",
-        "N": "Nitrogen",
-        "O": "Oxygen",
-        "F": "Fluorine",
-        "Ne": "Neon",
+    elements := map[string]map[string]string{
+        "H": map[string]string{
+            "name":"Hydrogen",
+            "state":"gas",
+        },
+        "He": map[string]string{
+            "name":"Helium",
+            "state":"gas",
+        },
+        "Li": map[string]string{
+            "name":"Lithium",
+            "state":"solid",
+        },
+        "Be": map[string]string{
+            "name":"Beryllium",
+            "state":"solid",
+        },
+        "B": map[string]string{
+            "name":"Boron",
+            "state":"solid",
+        },
+        "C": map[string]string{
+            "name":"Carbon",
+            "state":"solid",
+        },
+        "N": map[string]string{
+            "name":"Nitrogen",
+            "state":"gas",
+        },
+        "O": map[string]string{
+            "name":"Oxygen",
+            "state":"gas",
+        },
+        "F": map[string]string{
+            "name":"Fluorine",
+            "state":"gas",
+        },
+        "Ne": map[string]string{
+            "name":"Neon",
+            "state":"gas",
+        },
     }
 
-    if name, ok := elements["Be"]; ok {
-        fmt.Println(name, ok)
+    if el, ok := elements["Li"]; ok {
+        fmt.Println(el["name"], el["state"])
     }
 }
