5ba5d3df335a3b64da7384b982baa789491118df
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 14:51:37 -0400

    Chapter 6, step 15

--- golang-book/chapter-6/main.go
+++ golang-book/chapter-6/main.go
@@ -3,50 +3,22 @@ package main
 import "fmt"
 
 func main() {
-    elements := map[string]map[string]string{
-        "H": map[string]string{
-            "name":"Hydrogen",
-            "state":"gas",
-        },
-        "He": map[string]string{
-            "name":"Helium",
-            "state":"gas",
-        },
-        "Li": map[string]string{
-            "name":"Lithium",
-            "state":"solid",
-        },
-        "Be": map[string]string{
-            "name":"Beryllium",
-            "state":"solid",
-        },
-        "B": map[string]string{
-            "name":"Boron",
-            "state":"solid",
-        },
-        "C": map[string]string{
-            "name":"Carbon",
-            "state":"solid",
-        },
-        "N": map[string]string{
-            "name":"Nitrogen",
-            "state":"gas",
-        },
-        "O": map[string]string{
-            "name":"Oxygen",
-            "state":"gas",
-        },
-        "F": map[string]string{
-            "name":"Fluorine",
-            "state":"gas",
-        },
-        "Ne": map[string]string{
-            "name":"Neon",
-            "state":"gas",
-        },
+    var min int
+    x := []int{
+        48,96,86,68,
+        57,82,63,70,
+        37,34,83,27,
+        19,97, 9,17,
     }
 
-    if el, ok := elements["Li"]; ok {
-        fmt.Println(el["name"], el["state"])
+    for i, value := range x {
+        if i == 0 {
+            min = value
+        }
+
+        if value < min {
+            min = value
+        }
     }
+    fmt.Println(min)
 }
