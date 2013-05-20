6b433377a132d5881304cef124197ffa4673b031
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 20:27:49 -0400

    Chapter 10, step 3

--- golang-book/chapter-10/main.go
+++ golang-book/chapter-10/main.go
@@ -3,21 +3,28 @@ package main
 import (
     "fmt"
     "time"
-    "math/rand"
 )
 
-func f(n int) {
-    for i := 0; i < 10; i++ {
-        fmt.Println(n, ":", i)
-        amt := time.Duration(rand.Intn(250))
-        time.Sleep(time.Millisecond * amt)
+func pinger(c chan string) {
+    for i := 0; ; i++ {
+        c <- "ping"
+    }
+}
+
+func printer(c chan string) {
+    for {
+        msg := <- c
+        fmt.Println(msg)
+        time.Sleep(time.Second * 1)
     }
 }
 
 func main() {
-    for i := 0; i < 10; i++ {
-        go f(i)
-    }
+    var c chan string = make(chan string)
+
+    go pinger(c)
+    go printer(c)
+
     var input string
     fmt.Scanln(&input)
 }
