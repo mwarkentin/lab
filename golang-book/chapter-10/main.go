fb5e1b1eb3168dc4e94b5b2806d2bd094c6a764e
Michael Warkentin <mwarkentin@gmail.com>
2013-05-19 20:34:39 -0400

    Chapter 10, step 6

--- golang-book/chapter-10/main.go
+++ golang-book/chapter-10/main.go
@@ -25,11 +25,33 @@ func printer(c <-chan string) {
 }
 
 func main() {
-    var c chan string = make(chan string)
+    c1 := make(chan string)
+    c2 := make(chan string)
 
-    go pinger(c)
-    go ponger(c)
-    go printer(c)
+    go func() {
+        for {
+            c1 <- "from 1"
+            time.Sleep(time.Second * 2)
+        }
+    }()
+
+    go func() {
+        for {
+            c2 <- "from 2"
+            time.Sleep(time.Second * 3)
+        }
+    }()
+
+    go func() {
+        for {
+            select {
+            case msg1 := <- c1:
+                fmt.Println(msg1)
+            case msg2 := <- c2:
+                fmt.Println(msg2)
+            }
+        }
+    }()
 
     var input string
     fmt.Scanln(&input)
