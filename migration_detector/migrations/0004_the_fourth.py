610f48ebcc9151c9ab4430628a4206038e1079e8
Michael Warkentin <mwarkentin@gmail.com>
2019-03-22 08:59:22 -0400

    Fix 4th migration

--- migration_detector/migrations/0004_the_fourth.py
+++ migration_detector/migrations/0004_the_fourth.py
@@ -2,7 +2,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [('migrations', '0002_the_second')]
+    dependencies = [('migrations', '0004_the_fourth')]
 
     operations = [
         migrations.DeleteModel('Tribble'),
